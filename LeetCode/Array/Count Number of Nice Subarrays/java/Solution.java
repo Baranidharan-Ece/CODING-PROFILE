aec0679cf5fd2cc3f5d6adc609fc32cd645d743a
Baranidharan S <baranidharansnkdr@gmail.com>
2026-08-27 14:13:20 +0530

    feat: Add Count Number of Nice Subarrays

--- LeetCode/Array/Count Number of Nice Subarrays/java/Solution.java
+++ LeetCode/Array/Count Number of Nice Subarrays/java/Solution.java
@@ -1,16 +1,20 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int n = nums.length;
-        int[] cnt = new int[n + 1];
-        cnt[0] = 1;
-        int ans = 0, t = 0;
-        for (int v : nums) {
-            t += v & 1;
-            if (t - k >= 0) {
-                ans += cnt[t - k];
-            }
-            cnt[t]++;
+        int n = 0;
+        int c = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for (int x : nums) {
+
+            n += x % 2;
+
+            c += map.getOrDefault(n - k, 0);
+
+            map.put(n, map.getOrDefault(n, 0) + 1);
         }
-        return ans;
+
+        return c;
     }
 }
